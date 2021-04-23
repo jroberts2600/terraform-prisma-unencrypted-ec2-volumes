01ca9601b994b8592ed47b2439e36aa844f26709
Joe Buhr <jtb75@users.noreply.github.com>
2021-04-23 14:26:36 -0500

    Update rql_search.tf

--- rql_search.tf
+++ rql_search.tf
@@ -1,17 +1,17 @@
-resource "prismacloud_saved_search" "terraform_example" {
-    name = "Made by Terraform"
-    description = "made by terraform"
-    search_id = prismacloud_rql_search.terraform_example.search_id
-    query = prismacloud_rql_search.terraform_example.query
+resource "prismacloud_saved_search" "example" {
+    name = "TF - EC2 Volumes are unencrypted"
+    description = "Made by Terraform"
+    search_id = prismacloud_rql_search.x.search_id
+    query = prismacloud_rql_search.x.query
     time_range {
         relative {
-            unit = prismacloud_rql_search.terraform_example.time_range.0.relative.0.unit
-            amount = prismacloud_rql_search.terraform_example.time_range.0.relative.0.amount
+            unit = prismacloud_rql_search.x.time_range.0.relative.0.unit
+            amount = prismacloud_rql_search.x.time_range.0.relative.0.amount
         }
     }
 }
 
-resource "prismacloud_rql_search" "terraform_example" {
+resource "prismacloud_rql_search" "x" {
     search_type = "config"
     query = "config from cloud.resource where cloud.type = 'aws' AND api.name = 'aws-ec2-describe-volumes' AND json.rule = 'encrypted is false'"
     time_range {
