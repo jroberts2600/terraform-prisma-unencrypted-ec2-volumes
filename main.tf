e69a099a0dc9eb325ac915c1e3c828e2513db350
Joe Buhr <jtb75@users.noreply.github.com>
2021-04-23 17:52:41 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
     required_providers {
         prismacloud = {
             source = "PaloAltoNetworks/prismacloud"
-            version = "1.1.1"
+            version = "1.1.2"
         }
     }
 }
