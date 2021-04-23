3463fbf52ac9c3a0bf336f06d2f3a1b554e6ad64
Joe Buhr <jtb75@users.noreply.github.com>
2021-04-23 17:54:41 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,3 +16,9 @@ provider "prismacloud" {
     password = var.pc_secret_key
     protocol = "https"
 }
+
+# Set random string for use to unique names
+resource "random_string" "suffix" {
+    length  = 8
+    special = false
+}
