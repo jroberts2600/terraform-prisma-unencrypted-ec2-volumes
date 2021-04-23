cae39f147a39b766d8fbc22c619086d3e693cff4
Joe Buhr <jtb75@users.noreply.github.com>
2021-04-23 16:02:18 -0500

    Update rql_search.tf

--- rql_search.tf
+++ rql_search.tf
@@ -1,3 +1,16 @@
+
+resource "prismacloud_rql_search" "x" {
+    search_type = "config"
+    query = "config from cloud.resource where api.name = 'aws-ec2-describe-instances'"
+    time_range {
+        relative {
+            unit = "hour"
+            amount = 24
+        }
+    }
+}
+
+
 resource "prismacloud_saved_search" "example_search" {
     name = "TF - Example Saved Search"
     description = "TF - Example Saved Search"
@@ -11,13 +24,3 @@ resource "prismacloud_saved_search" "example_search" {
     }
 }
 
-resource "prismacloud_rql_search" "x" {
-    search_type = "config"
-    query = "config from cloud.resource where api.name = 'aws-ec2-describe-instances'"
-    time_range {
-        relative {
-            unit = "hour"
-            amount = 24
-        }
-    }
-}
