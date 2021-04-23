aa4aa05cb7dce91fd2a8967ec9a07b9d42ea8a7d
Joe Buhr <jtb75@users.noreply.github.com>
2021-04-23 18:39:00 -0500

    Update rql_search.tf

--- rql_search.tf
+++ rql_search.tf
@@ -1,17 +1,17 @@
 resource "prismacloud_saved_search" "example_search" {
     name = "TF - Example Saved Search"
     description = "TF - Example Saved Search"
-    search_id = prismacloud_rql_search.x.search_id
-    query = prismacloud_rql_search.x.query
+    search_id = prismacloud_rql_search.unencrypted_ec2_rql.search_id
+    query = prismacloud_rql_search.unencrypted_ec2_rql.query
     time_range {
         relative {
-            unit = prismacloud_rql_search.x.time_range.0.relative.0.unit
-            amount = prismacloud_rql_search.x.time_range.0.relative.0.amount
+            unit = prismacloud_rql_search.unencrypted_ec2_rql.time_range.0.relative.0.unit
+            amount = prismacloud_rql_search.unencrypted_ec2_rql.time_range.0.relative.0.amount
         }
     }
 }
 
-resource "prismacloud_rql_search" "x" {
+resource "prismacloud_rql_search" "unencrypted_ec2_rql" {
     search_type = "config"
     query = "config from cloud.resource where cloud.type = 'aws' AND api.name = 'aws-ec2-describe-volumes' AND json.rule = 'encrypted is false'"
     time_range {
