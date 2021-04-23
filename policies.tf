45aa41dafe040083e0f9a3e3f3de9d0fa1ab9143
Joe Buhr <jtb75@users.noreply.github.com>
2021-04-23 14:30:21 -0500

    Update policies.tf

--- policies.tf
+++ policies.tf
@@ -1,12 +1,12 @@
-resource "prismacloud_policy" "test_policy" {
-    name = "test_policy"
+resource "prismacloud_policy" "example_policy" {
+    name = "EC2 Volumes with unencrypted volumes"
     policy_type = "config"
     description = "This is a test of Terraform source-controlled Prisma policies"
     severity = "low"
     cloud_type = "aws"
     rule {
-        name = "test_policy"
-        criteria = prismacloud_saved_search.terraform_example.search_id
+        name = "rule"
+        criteria = "prismacloud_saved_search.example.search_id"
         parameters = {
             "savedSearch": "true",
             "withIac": "false",
