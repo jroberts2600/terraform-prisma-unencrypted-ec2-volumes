0fd10739b6b08622db3639cca3fc5bbe1531df28
Joe Buhr <jtb75@users.noreply.github.com>
2021-04-23 16:18:35 -0500

    Update rql_search.tf

--- rql_search.tf
+++ rql_search.tf
@@ -4,7 +4,7 @@ resource "random_string" "suffix" {
 }
 
 resource "prismacloud_saved_search" "example_search" {
-    name = "TF - Example Saved Search - ${random_string}"
+    name = "TF - Example Saved Search - 3hf78"
     description = "TF - Example Saved Search"
     search_id = prismacloud_rql_search.x.search_id
     query = prismacloud_rql_search.x.query
