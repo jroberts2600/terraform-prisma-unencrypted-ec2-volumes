eee09eda0acf9a3ec2f8618f4c599819f60037a0
Joe Buhr <jtb75@users.noreply.github.com>
2021-04-23 19:22:35 -0500

    Updated policies

--- policies.tf
+++ policies.tf
@@ -1,12 +1,12 @@
-resource "prismacloud_policy" "test_policy" {
+resource "prismacloud_policy" "uv_policy" {
   name = "test_policy"
   policy_type = "config"
-  description = "This is a test of Terraform source-controlled Prisma policies"
+  description = "Unencrypted EC2 Volumes - TF Managed"
   severity = "low"
   cloud_type = "aws"
   rule {
     name = "test_policy"
-    criteria = prismacloud_saved_search.terraform_example.search_id
+    criteria = prismacloud_saved_search.uv_ss.search_id
     parameters = {
       "savedSearch": "true",
       "withIac": "false",
